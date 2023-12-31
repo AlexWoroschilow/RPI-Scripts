31a54790f54ff0a0a5dc065bc29eba5b4dcd2f50
AlexWoroschilow <alex.woroschilow@gmail.com>
2023-12-31 17:08:58 +0100

    =bugfix

--- src/main.py
+++ src/main.py
@@ -14,13 +14,13 @@ mlx = adafruit_mlx90640.MLX90640(i2c) # begin MLX90640 with I2C comm
 mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_16_HZ # set refresh rate
 mlx_shape = (24,32) # mlx90640 shape
 
-mlx_interp_val = 10 # interpolate # on each dimension
+mlx_interp_val = 1 # interpolate # on each dimension
 mlx_interp_shape = (mlx_shape[0]*mlx_interp_val,
                     mlx_shape[1]*mlx_interp_val) # new shape
 
 fig = plt.figure(figsize=(12,9)) # start figure
 ax = fig.add_subplot(111) # add subplot
-fig.subplots_adjust(0.05,0.05,0.95,0.95) # get rid of unnecessary padding
+fig.subplots_adjust(0,0,0.99,0.99) # get rid of unnecessary padding
 therm1 = ax.imshow(np.zeros(mlx_interp_shape),interpolation='none',
                    cmap=plt.cm.bwr,vmin=25,vmax=45) # preemptive image
 cbar = fig.colorbar(therm1) # setup colorbar
@@ -37,12 +37,18 @@ def plot_update():
     data_array = np.fliplr(np.reshape(frame,mlx_shape)) # reshape, flip data
     data_array = ndimage.zoom(data_array,mlx_interp_val) # interpolate
     therm1.set_array(data_array) # set data
-    therm1.set_clim(vmin=np.min(data_array),vmax=np.max(data_array)) # set bounds
+    #therm1.set_clim(vmin=np.min(data_array),vmax=np.max(data_array)) # set bounds
+    #therm1.set_clim(vmin=25,vmax=45) # set bounds
+
+
     if hasattr(cbar, "on_mappable_changed"):
         cbar.on_mappable_changed(therm1) # update colorbar range
 
     ax.draw_artist(therm1) # draw new thermal image
+
     fig.canvas.blit(ax.bbox) # draw background
+
+    fig.canvas.draw()
     fig.canvas.flush_events() # show the new image
     return
 
